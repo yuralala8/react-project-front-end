9c5b9b4ed13ac5673a629bd93da42657287e5cd9
Flatiron School <flatironschool@Flatirons-MacBook-Air-3.local>
2017-10-18 18:24:53 -0400

    moved suggestion box outside of search list

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,11 +5,14 @@ import SuggestionList from './SuggestionList'
 
 const Home = (props) => {
 	return (
-			<div className="search-top">
-				<Search getRestaurantData = {props.getRestaurantData}/>
-		       <RestaurantList roomKey={props.roomKey} handleClick = {props.handleClick} restaurants={props.restaurants}/>
-		       <SuggestionList roomKey={props.roomKey} suggestions = {props.suggestions} voteChange={props.voteChange} currentUser={props.currentUser} deleteSugg={props.deleteSugg}/>
-      </div>
+			<div>
+				<div className="search-top">
+					<Search getRestaurantData = {props.getRestaurantData}/>
+						 <RestaurantList roomKey={props.roomKey} handleClick = {props.handleClick} restaurants={props.restaurants}/>
+				</div>
+				<SuggestionList roomKey={props.roomKey} suggestions = {props.suggestions} voteChange={props.voteChange} currentUser={props.currentUser} deleteSugg={props.deleteSugg}/>
+			</div>
+
       )
 
 }
